27b5ae72ffd8382953116f51812f6f187c775f9e
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-19 21:44:25 +0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+import { Board, Winner } from './Board';
+import { ResetScreen } from './ResetScreen';
+import { StartScreen } from './StartScreen';
+
+const variants = {
+	game: {
+		opacity: 1,
+		scale: 1,
+		width: '500px',
+		height: '500px',
+		transition: {
+			type: 'spring',
+			duration: 0.8,
+		},
+	},
+	start: {
+		opacity: 1,
+		scale: 1,
+		width: '200px',
+		height: '100px',
+		transition: { type: 'spring', duration: 0.8 },
+	},
+	reset: {
+		opacity: 1,
+		scale: 1,
+		width: '350px',
+		height: '300px',
+		transition: { type: 'spring', duration: 0.8 },
+	},
+	hidden: { opacity: 0, scale: 0.8 },
+};
+
+const BoardContainer = styled(motion.div).attrs(() => ({
+	initial: 'hidden',
+	variants,
+}))`
+	background: #fff;
+	border-radius: 16px;
+	box-shadow: -6px 10px 30px 4px #00000033;
+	border: 20px solid #fff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+type GameState = 'start' | 'game' | 'reset';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [winner, setWinner] = useState<Winner>();
+	const [gameState, setGameState] = useState<GameState>('start');
+
+	const onStart = () => {
+		setGameState('game');
+	};
+
+	const onGameEnd = (winer: Winner) => {
+		setWinner(winer);
+		setGameState('reset');
+	};
+
+	const onReset = () => {
+		setWinner(undefined);
+		setGameState('game');
+	};
+
+	return (
+		<>
+			<h1>Typescript Tic-Tac-Toe</h1>
+			<BoardContainer animate={gameState}>
+				{
+					{
+						start: <StartScreen onStart={onStart} />,
+						game: <Board onGameEnd={onGameEnd} />,
+						reset: <ResetScreen winner={winner} onReset={onReset} />,
+					}[gameState]
+				}
+			</BoardContainer>
+		</>
+	);
 }
 
 export default App;
